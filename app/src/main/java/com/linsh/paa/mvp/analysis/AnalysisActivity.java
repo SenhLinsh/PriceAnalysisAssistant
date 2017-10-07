0c9b577ba1040cc57f5b15aaa7e07f8cc8dba576
SenhLinsh <lyuanxiang0812@126.com>
2017-10-07 17:43:53 +0800

    调整 价格分析界面表格样式

--- app/src/main/java/com/linsh/paa/mvp/analysis/AnalysisActivity.java
+++ app/src/main/java/com/linsh/paa/mvp/analysis/AnalysisActivity.java
@@ -4,16 +4,21 @@ import android.graphics.Color;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.linsh.lshapp.common.base.BaseViewActivity;
 import com.linsh.lshutils.module.SimpleDate;
 import com.linsh.lshutils.utils.LshActivityUtils;
 import com.linsh.paa.R;
+import com.linsh.paa.view.MyMarkerView;
 
 import java.util.ArrayList;
 
@@ -36,17 +41,24 @@ public class AnalysisActivity extends BaseViewActivity<AnalysisContract.Presente
     protected void initView() {
         lineChart = (LineChart) findViewById(R.id.lc_analysis_chart);
 
+        lineChart.getDescription().setEnabled(false);
+        lineChart.setGridBackgroundColor(Color.argb(150, 51, 181, 229));
+        lineChart.setDrawGridBackground(true);
+        lineChart.setPinchZoom(false);
+        lineChart.setDrawBorders(true);
+        lineChart.setBorderColor(Color.argb(50, 0, 0, 0));
+        Legend l = lineChart.getLegend();
+        l.setEnabled(false);
+
+        MyMarkerView mv = new MyMarkerView(this);
+        mv.setChartView(lineChart);
+        lineChart.setMarker(mv);
+
+
         //获取此图表的x轴
         XAxis xAxis = lineChart.getXAxis();
-        xAxis.setEnabled(true); // 设置轴启用或禁用 如果禁用以下的设置全部不生效
-        xAxis.setDrawAxisLine(true); // 是否绘制轴线
-        xAxis.setDrawGridLines(true); // 设置x轴上每个点对应的线
-        xAxis.setDrawLabels(true); // 绘制标签  指x轴上的对应数值
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // 设置x轴的显示位置
-        xAxis.enableGridDashedLine(10f, 10f, 0f); // 设置竖线的显示样式为虚线
-        xAxis.setAvoidFirstLastClipping(true); // 图表将避免第一个和最后一个标签条目被减掉在图表或屏幕的边缘
-        xAxis.setLabelRotationAngle(10f);//设置x轴标签的旋转角度
-        xAxis.setSpaceMin(1000L * 60 * 60 * 24);
+        xAxis.setDrawGridLines(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
@@ -57,13 +69,12 @@ public class AnalysisActivity extends BaseViewActivity<AnalysisContract.Presente
 
         //获取右边的轴线
         YAxis rightAxis = lineChart.getAxisRight();
-        rightAxis.setEnabled(false); // 设置图表右边的y轴禁用
+        rightAxis.setEnabled(false);
+
         //获取左边的轴线
         YAxis leftAxis = lineChart.getAxisLeft();
-        leftAxis.enableGridDashedLine(10f, 10f, 0f); // 设置网格线为虚线效果
-        leftAxis.setDrawZeroLine(false); // 是否绘制0所在的网格线
-        leftAxis.setAxisMinimum(0);
-        leftAxis.setSpaceMin(1);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawZeroLine(false);
         leftAxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
@@ -79,8 +90,6 @@ public class AnalysisActivity extends BaseViewActivity<AnalysisContract.Presente
 
     @Override
     public void setData(ArrayList<Entry> lowPrices, ArrayList<Entry> highPrices) {
-//        lineChart.getXAxis().setAxisMinimum(lowPrices.get(0).getX() - 1000L * 60 * 60 * 24);
-
         LineDataSet lowSet = null;
         LineDataSet highSet = null;
         if (lineChart.getData() != null && lineChart.getData().getDataSetCount() == 1) {
@@ -90,36 +99,48 @@ public class AnalysisActivity extends BaseViewActivity<AnalysisContract.Presente
             highSet = (LineDataSet) lineChart.getData().getDataSetByIndex(1);
         }
         if (lowSet == null) {
-            lowSet = new LineDataSet(lowPrices, "价格");
-            lowSet.setColor(Color.BLACK);
-            lowSet.setCircleColor(Color.BLACK);
-            lowSet.setLineWidth(1f); // 设置线宽
-            lowSet.setCircleRadius(3f); // 设置焦点圆心的大小
-            lowSet.enableDashedHighlightLine(10f, 5f, 0f); // 点击后的高亮线的显示样式
-            lowSet.setHighlightLineWidth(2f); // 设置点击交点后显示高亮线宽
-            lowSet.setHighlightEnabled(true); // 是否禁用点击高亮线
-            lowSet.setHighLightColor(Color.RED); // 设置点击交点后显示交高亮线的颜色
-            lowSet.setValueTextSize(9f); // 设置显示值的文字大小
-            lowSet.setDrawFilled(false); // 设置禁用范围背景填充
-            LineData lowData = new LineData();
-            lowData.addDataSet(lowSet);
-            lineChart.setData(lowData);
+            lowSet = new LineDataSet(lowPrices, "价格下限");
+            lowSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+            lowSet.setColor(Color.rgb(255, 241, 46));
+            lowSet.setCircleColor(Color.rgb(244, 117, 117));
+            lowSet.setCircleRadius(3f);
+            lowSet.setLineWidth(2f);
+            lowSet.setFillAlpha(255);
+            lowSet.setDrawFilled(true);
+            lowSet.setFillColor(Color.WHITE);
+            lowSet.setHighLightColor(Color.rgb(244, 117, 117));
+            lowSet.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return lineChart.getAxisLeft().getAxisMinimum();
+                }
+            });
+            LineData data = new LineData();
+            data.addDataSet(lowSet);
+            data.setDrawValues(false);
+            lineChart.setData(data);
         } else {
             lowSet.setValues(lowPrices);
         }
         if (highSet == null && highPrices != null) {
             highSet = new LineDataSet(highPrices, "价格上限");
-            highSet.setColor(Color.BLACK);
-            highSet.setCircleColor(Color.BLACK);
-            highSet.setLineWidth(1f); // 设置线宽
-            highSet.setCircleRadius(3f); // 设置焦点圆心的大小
-            highSet.enableDashedHighlightLine(10f, 5f, 0f); // 点击后的高亮线的显示样式
-            highSet.setHighlightLineWidth(2f); // 设置点击交点后显示高亮线宽
-            highSet.setHighlightEnabled(true); // 是否禁用点击高亮线
-            highSet.setHighLightColor(Color.RED); // 设置点击交点后显示交高亮线的颜色
-            highSet.setValueTextSize(9f); // 设置显示值的文字大小
-            highSet.setDrawFilled(false); // 设置禁用范围背景填充
+            highSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+            highSet.setColor(Color.rgb(255, 241, 46));
+            highSet.setCircleColor(Color.rgb(244, 117, 117));
+            highSet.setCircleRadius(3f);
+            highSet.setLineWidth(2f);
+            highSet.setFillAlpha(255);
+            highSet.setDrawFilled(true);
+            highSet.setFillColor(Color.WHITE);
+            highSet.setHighLightColor(Color.rgb(244, 117, 117));
+            highSet.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return lineChart.getAxisLeft().getAxisMaximum();
+                }
+            });
             lineChart.getData().addDataSet(highSet);
+            lineChart.getData().setDrawValues(false);
         } else if (highSet != null && highPrices != null) {
             highSet.setValues(lowPrices);
         } else if (highSet != null) {
---
+++ app/src/main/java/com/linsh/paa/view/MyMarkerView.java
@@ -0,0 +1,50 @@
+package com.linsh.paa.view;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Utils;
+import com.linsh.paa.R;
+
+/**
+ * Custom implementation of the MarkerView.
+ * 
+ * @author Philipp Jahoda
+ */
+public class MyMarkerView extends MarkerView {
+
+    private TextView tvContent;
+
+    public MyMarkerView(Context context) {
+        super(context, R.layout.view_marker);
+        tvContent = (TextView) findViewById(R.id.tvContent);
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+
+        if (e instanceof CandleEntry) {
+
+            CandleEntry ce = (CandleEntry) e;
+
+            tvContent.setText("" + Utils.formatNumber(ce.getHigh(), 0, true));
+        } else {
+
+            tvContent.setText("" + Utils.formatNumber(e.getY(), 0, true));
+        }
+
+        super.refreshContent(e, highlight);
+    }
+
+    @Override
+    public MPPointF getOffset() {
+        return new MPPointF(-(getWidth() / 2), -getHeight());
+    }
+}
