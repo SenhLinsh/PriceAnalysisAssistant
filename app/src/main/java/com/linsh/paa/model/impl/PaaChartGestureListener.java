ec4d5246e59965054e959f40d1d5500f64eab350
SenhLinsh <lyuanxiang0812@126.com>
2017-10-08 03:31:01 +0800

    完善 价格分析界面表格

---
+++ app/src/main/java/com/linsh/paa/model/impl/PaaChartGestureListener.java
@@ -0,0 +1,56 @@
+package com.linsh.paa.model.impl;
+
+import android.view.MotionEvent;
+
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+
+/**
+ * <pre>
+ *    author : Senh Linsh
+ *    date   : 2017/10/08
+ *    desc   :
+ * </pre>
+ */
+public class PaaChartGestureListener implements OnChartGestureListener {
+
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
+    }
+}
--- app/src/main/java/com/linsh/paa/mvp/analysis/AnalysisActivity.java
+++ app/src/main/java/com/linsh/paa/mvp/analysis/AnalysisActivity.java
@@ -14,18 +14,22 @@ import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IFillFormatter;
 import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.linsh.lshapp.common.base.BaseViewActivity;
 import com.linsh.lshutils.module.SimpleDate;
+import com.linsh.lshutils.utils.Basic.LshLogUtils;
 import com.linsh.lshutils.utils.LshActivityUtils;
 import com.linsh.paa.R;
 import com.linsh.paa.view.MyMarkerView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AnalysisActivity extends BaseViewActivity<AnalysisContract.Presenter>
         implements AnalysisContract.View {
 
-    private LineChart lineChart;
+    private LineChart mLineChart;
 
     @Override
     protected AnalysisContract.Presenter initPresenter() {
@@ -39,40 +43,48 @@ public class AnalysisActivity extends BaseViewActivity<AnalysisContract.Presente
 
     @Override
     protected void initView() {
-        lineChart = (LineChart) findViewById(R.id.lc_analysis_chart);
+        mLineChart = (LineChart) findViewById(R.id.lc_analysis_chart);
 
-        lineChart.getDescription().setEnabled(false);
-        lineChart.setGridBackgroundColor(Color.argb(150, 51, 181, 229));
-        lineChart.setDrawGridBackground(true);
-        lineChart.setPinchZoom(false);
-        lineChart.setDrawBorders(true);
-        lineChart.setBorderColor(Color.argb(50, 0, 0, 0));
-        Legend l = lineChart.getLegend();
+        mLineChart.getDescription().setEnabled(false);
+        mLineChart.setGridBackgroundColor(Color.argb(150, 51, 181, 229));
+        mLineChart.setDrawGridBackground(true);
+        mLineChart.setPinchZoom(false);
+        mLineChart.setDrawBorders(true);
+        mLineChart.setBorderColor(Color.argb(50, 0, 0, 0));
+        Legend l = mLineChart.getLegend();
         l.setEnabled(false);
 
         MyMarkerView mv = new MyMarkerView(this);
-        mv.setChartView(lineChart);
-        lineChart.setMarker(mv);
-
+        mv.setChartView(mLineChart);
+        mLineChart.setMarker(mv);
 
         //获取此图表的x轴
-        XAxis xAxis = lineChart.getXAxis();
+        XAxis xAxis = mLineChart.getXAxis();
         xAxis.setDrawGridLines(false);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                return new SimpleDate((long) value)
-                        .getNormalizedString(false);
+                float curRange = mLineChart.getVisibleXRange();
+                if (curRange < 1000L * 60 * 60 * 24 * 3) {
+                    return new SimpleDateFormat("dd日H时").format(new Date((long) value));
+                } else if (curRange < 1000L * 60 * 60 * 24 * 30 * 3) {
+                    return new SimpleDate((long) value).getNormalizedString(false);
+                } else if (curRange < 1000L * 60 * 60 * 24 * 30 * 12 * 3) {
+                    SimpleDate simpleDate = new SimpleDate((long) value);
+                    return String.format("%s年%s月", simpleDate.getYear(), simpleDate.getMonth());
+                } else {
+                    return new SimpleDate((long) value).getYear() + "年";
+                }
             }
         });
 
         //获取右边的轴线
-        YAxis rightAxis = lineChart.getAxisRight();
+        YAxis rightAxis = mLineChart.getAxisRight();
         rightAxis.setEnabled(false);
 
         //获取左边的轴线
-        YAxis leftAxis = lineChart.getAxisLeft();
+        YAxis leftAxis = mLineChart.getAxisLeft();
         leftAxis.setDrawGridLines(false);
         leftAxis.setDrawZeroLine(false);
         leftAxis.setValueFormatter(new IAxisValueFormatter() {
@@ -92,59 +104,107 @@ public class AnalysisActivity extends BaseViewActivity<AnalysisContract.Presente
     public void setData(ArrayList<Entry> lowPrices, ArrayList<Entry> highPrices) {
         LineDataSet lowSet = null;
         LineDataSet highSet = null;
-        if (lineChart.getData() != null && lineChart.getData().getDataSetCount() == 1) {
-            lowSet = (LineDataSet) lineChart.getData().getDataSetByIndex(0);
-        } else if (lineChart.getData() != null && lineChart.getData().getDataSetCount() == 2) {
-            lowSet = (LineDataSet) lineChart.getData().getDataSetByIndex(0);
-            highSet = (LineDataSet) lineChart.getData().getDataSetByIndex(1);
+        if (mLineChart.getData() != null && mLineChart.getData().getDataSetCount() == 1) {
+            lowSet = (LineDataSet) mLineChart.getData().getDataSetByIndex(0);
+        } else if (mLineChart.getData() != null && mLineChart.getData().getDataSetCount() == 2) {
+            lowSet = (LineDataSet) mLineChart.getData().getDataSetByIndex(0);
+            highSet = (LineDataSet) mLineChart.getData().getDataSetByIndex(1);
         }
-        if (lowSet == null) {
-            lowSet = new LineDataSet(lowPrices, "价格下限");
-            lowSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-            lowSet.setColor(Color.rgb(255, 241, 46));
-            lowSet.setCircleColor(Color.rgb(244, 117, 117));
-            lowSet.setCircleRadius(3f);
-            lowSet.setLineWidth(2f);
-            lowSet.setFillAlpha(255);
-            lowSet.setDrawFilled(true);
-            lowSet.setFillColor(Color.WHITE);
-            lowSet.setHighLightColor(Color.rgb(244, 117, 117));
-            lowSet.setFillFormatter(new IFillFormatter() {
-                @Override
-                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
-                    return lineChart.getAxisLeft().getAxisMinimum();
-                }
-            });
-            LineData data = new LineData();
-            data.addDataSet(lowSet);
-            data.setDrawValues(false);
-            lineChart.setData(data);
+
+        if (lowPrices == null) {
+            LshLogUtils.e("lowPrices == null");
+        } else if (highPrices == null) {
+            if (highSet != null) {
+                mLineChart.getData().removeDataSet(1);
+            }
+            if (lowSet == null) {
+                lowSet = new LineDataSet(lowPrices, "价格");
+                lowSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                lowSet.setColor(Color.WHITE);
+                lowSet.setCircleColor(Color.rgb(244, 117, 117));
+                lowSet.setCircleRadius(3f);
+                lowSet.setLineWidth(2f);
+                lowSet.setHighLightColor(Color.rgb(244, 117, 117));
+                LineData data = new LineData();
+                data.addDataSet(lowSet);
+                data.setDrawValues(false);
+                mLineChart.setData(data);
+            } else {
+                lowSet.setValues(lowPrices);
+            }
         } else {
-            lowSet.setValues(lowPrices);
-        }
-        if (highSet == null && highPrices != null) {
-            highSet = new LineDataSet(highPrices, "价格上限");
-            highSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-            highSet.setColor(Color.rgb(255, 241, 46));
-            highSet.setCircleColor(Color.rgb(244, 117, 117));
-            highSet.setCircleRadius(3f);
-            highSet.setLineWidth(2f);
-            highSet.setFillAlpha(255);
-            highSet.setDrawFilled(true);
-            highSet.setFillColor(Color.WHITE);
-            highSet.setHighLightColor(Color.rgb(244, 117, 117));
-            highSet.setFillFormatter(new IFillFormatter() {
-                @Override
-                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
-                    return lineChart.getAxisLeft().getAxisMaximum();
+            if (lowSet == null) {
+                if (highSet != null) {
+                    mLineChart.getData().removeDataSet(1);
                 }
-            });
-            lineChart.getData().addDataSet(highSet);
-            lineChart.getData().setDrawValues(false);
-        } else if (highSet != null && highPrices != null) {
-            highSet.setValues(lowPrices);
-        } else if (highSet != null) {
-            lineChart.getData().removeDataSet(1);
+                lowSet = new LineDataSet(lowPrices, "价格下限");
+                lowSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                lowSet.setColor(Color.rgb(255, 241, 46));
+                lowSet.setCircleColor(Color.rgb(244, 117, 117));
+                lowSet.setCircleRadius(3f);
+                lowSet.setLineWidth(2f);
+                lowSet.setFillAlpha(255);
+                lowSet.setDrawFilled(true);
+                lowSet.setFillColor(Color.WHITE);
+                lowSet.setHighLightColor(Color.rgb(244, 117, 117));
+                lowSet.setFillFormatter(new IFillFormatter() {
+                    @Override
+                    public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                        return mLineChart.getAxisLeft().getAxisMinimum();
+                    }
+                });
+                highSet = new LineDataSet(highPrices, "价格上限");
+                highSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                highSet.setColor(Color.rgb(255, 241, 46));
+                highSet.setCircleColor(Color.rgb(244, 117, 117));
+                highSet.setCircleRadius(3f);
+                highSet.setLineWidth(2f);
+                highSet.setFillAlpha(255);
+                highSet.setDrawFilled(true);
+                highSet.setFillColor(Color.WHITE);
+                highSet.setHighLightColor(Color.rgb(244, 117, 117));
+                highSet.setFillFormatter(new IFillFormatter() {
+                    @Override
+                    public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                        return mLineChart.getAxisLeft().getAxisMaximum();
+                    }
+                });
+                LineData data = new LineData();
+                data.addDataSet(lowSet);
+                data.addDataSet(highSet);
+                data.setDrawValues(false);
+                mLineChart.setData(data);
+            } else {
+                lowSet.setValues(lowPrices);
+                if (highSet != null) {
+                    highSet.setValues(lowPrices);
+                } else {
+                    highSet = new LineDataSet(highPrices, "价格上限");
+                    highSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                    highSet.setColor(Color.rgb(255, 241, 46));
+                    highSet.setCircleColor(Color.rgb(244, 117, 117));
+                    highSet.setCircleRadius(3f);
+                    highSet.setLineWidth(2f);
+                    highSet.setFillAlpha(255);
+                    highSet.setDrawFilled(true);
+                    highSet.setFillColor(Color.WHITE);
+                    highSet.setHighLightColor(Color.rgb(244, 117, 117));
+                    highSet.setFillFormatter(new IFillFormatter() {
+                        @Override
+                        public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                            return mLineChart.getAxisLeft().getAxisMaximum();
+                        }
+                    });
+                    mLineChart.getData().addDataSet(highSet);
+                }
+            }
         }
+        mLineChart.getData().setDrawValues(false);
+        ViewPortHandler handler = mLineChart.getViewPortHandler();
+        float xRange = mLineChart.getXRange();
+        float minXRange = 1000L * 60 * 60 * 24;
+        handler.setMaximumScaleX(Math.max(xRange / minXRange, 1));
+        float yRange = mLineChart.getAxisLeft().mAxisRange;
+        handler.setMaximumScaleY(Math.max(yRange / 6, 1));
     }
 }
